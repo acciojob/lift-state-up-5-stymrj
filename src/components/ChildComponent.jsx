7f22dc6ac778c97c667b068da3c7b91c004dc8a8
Satyam Gupta <stymrj@Satyams-MacBook-Air.local>
2025-09-30 17:01:33 +0530

    commit

--- src/components/ChildComponent.jsx
+++ src/components/ChildComponent.jsx
@@ -6,15 +6,19 @@ const ChildComponent = ({setIsLoggedIn, isLoggedIn}) => {
     <div style={{display:'flex', flexDirection:'column'}}>
             {/* Do not remove the main div */}
             <h1>Parent Component</h1>
+            <form>
             <label>Username: </label>
             <input style={{border:'5px',height:'5vh',width:'20vw', background:"cyan", margin:'2px', fontSize:'20px'}}/>
             <label>Password: </label>
             <input style={{border:'5px',height:'5vh',width:'20vw', background:"gray", margin:'2px', fontSize:'20px'}}/>
             <button
+            
             onClick={()=>{
-              setIsLoggedIn(true)
-            }} 
-            style={{width:'20vw', height:'5vh', marginTop:'10px', backgroundColor:'blue', color:'white', fontSize:'20px'}}>Login</button>        
+                setIsLoggedIn(true)
+            }}
+
+            style={{width:'20vw', height:'5vh', marginTop:'10px', backgroundColor:'blue', color:'white', fontSize:'20px'}}>Login</button>
+            </form>        
         </div>
   )
 }
