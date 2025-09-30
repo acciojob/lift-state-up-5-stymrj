9476476b544e967f13675e63ceb5ece27b9c8c75
Satyam Gupta <stymrj@Satyams-MacBook-Air.local>
2025-09-30 17:16:08 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import './../styles/App.css';
 import ChildComponent from "./ChildComponent";
@@ -7,9 +6,11 @@ const App = () => {
   const[isLoggedIn,setIsLoggedIn] = useState(false)
   return (
     <div style={{display:'flex', flexDirection:'column', height:'100vh', width:'100vw', fontSize:'20px'}}>
-      {isLoggedIn?'You are loggedin!':
-      <ChildComponent isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>
-      }
+      { isLoggedIn ? (
+        <h1>You are loggedin!</h1>
+      ):
+      (<ChildComponent isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}/>)
+    }
     </div>
   )
 }
